6b588acab8390cabeb94e9fb76d518bfa559e3b3
Riswan01 <komplikasigemer2000m@gmail.com>
2024-05-31 00:06:54 +0700

    riswan : Melakukan Perbaikan di Storeplant Request

--- backend/identitas-laut/app/Models/plant.php
+++ backend/identitas-laut/app/Models/plant.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class plant extends Model
+class Plant extends Model
 {
     use HasFactory;
 }
